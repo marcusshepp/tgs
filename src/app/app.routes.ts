c1e797df929a659552fd7bc3406b801e8dded80c
marcusshepp <marcusshepdotcom@gmail.com>
2025-01-25 15:19:25 -0500

    catering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ReviewsComponent } from "./reviews/reviews.component";
+import { CateringComponent } from "./catering/catering.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "reviews", component: ReviewsComponent },
+  { path: "catering", component: CateringComponent },
 ];
