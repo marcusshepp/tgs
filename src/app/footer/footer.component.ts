96d372f6f3b8020fea423804fc399ff07dce3436
marcusshepp <marcusshepdotcom@gmail.com>
2025-02-02 11:49:24 -0500

    footer bar

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,19 @@
-import { Component } from "@angular/core";
-import { SOCIAL_MEDIA } from "../data/social-media.model";
-import { CONTACT } from "../data/contact-info.model";
+import { Component } from '@angular/core';
+import { SOCIAL_MEDIA } from '../data/social-media.model';
+import { CONTACT } from '../data/contact-info.model';
+import { RouterModule } from '@angular/router';
+import { MENU_ITEMS, MenuItem } from '../data/menu.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: "app-footer",
-  standalone: true,
-  imports: [],
-  templateUrl: "./footer.component.html",
-  styleUrl: "./footer.component.scss",
+    selector: 'app-footer',
+    standalone: true,
+    imports: [RouterModule, CommonModule],
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-  public socials = SOCIAL_MEDIA;
-  public contact = CONTACT;
+    public socials = SOCIAL_MEDIA;
+    public contact = CONTACT;
+    public menu: MenuItem[] = MENU_ITEMS.slice(0, 5);
 }
