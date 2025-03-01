37800611e52c01ce5ffce650369d12d83b7fc249
marcusshepp <marcusshepdotcom@gmail.com>
2025-03-01 00:14:27 -0500

    improvements ;D

--- src/app/contact/contact-us/contact-us.component.ts
+++ src/app/contact/contact-us/contact-us.component.ts
@@ -3,12 +3,9 @@ import { ContactFormComponent } from '../contact-form/contact-form.component';
 
 @Component({
     selector: 'app-contact-us',
+    standalone: true,
     imports: [ContactFormComponent],
     templateUrl: './contact-us.component.html',
     styleUrl: './contact-us.component.scss',
 })
-export class ContactUsComponent {
-    public menuClosed(isClosed: boolean): void {
-        console.log('Menu closed:', isClosed);
-    }
-}
+export class ContactUsComponent {}
